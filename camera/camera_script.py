56f25746f23b4a8573789dc256b91b9cdb4f119f
unknown <samueljoharris@gmail.com>
2019-05-30 13:52:46 -0400

    Creating Bash Script for Night!

--- camera/camera_script.py
+++ camera/camera_script.py
@@ -28,19 +28,11 @@ def main():
     logging.info(f"Creating SFTP to {server}")
     sftp_client = ssh.open_sftp()
 
+    ssh.exec_command(f"sudo systemctl stop timelapse")
+
     # lets put our current version of the file in the necessarsy location
     put_file = sftp_client.put(capture_local_file, capture_remote_file)
     sftp_client.chmod(capture_remote_file, put_file.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
-
-    # with the file in place we need to execute a command on the server to move it
-    # move_command = f"sudo mv {capture_remote_file} {hourly_cron} && sudo chown root:root {hourly_cron}"
-    # this will force a picture
-    # ssh.exec_command(move_command)
-
-    if 1 == 0:
-        ssh.exec_command(f"python  {capture_remote_file}")
-        time.sleep(30)
-    # lets make sure we are croning
 
     # who knows how many files there are so lets get them
     file_gen = sftp_client.listdir("/home/pi/camera")
@@ -54,6 +46,8 @@ def main():
 
         # now that we have the file delete it from the remote
         sftp_client.remove(remote_file)
+
+    ssh.exec_command(f"sudo systemctl restart timelapse")
 
     images = []
     output_images = os.listdir(local_output)
--- camera/timelapse_capture.sh
+++ camera/timelapse_capture.sh
@@ -1,4 +1,24 @@
-#!/bin/bash
+#!/bin/bash -
 
+current_hour=$(date +"%H")
+mode=auto
+output_dir=/home/pi/camera
+sleep_for=30 # 1800000 for 30 min takes
+width=1944 
+height=2592
 
-raspistill -e png -o /home/pi/camera/pland_%04d.jpg -mm backlit -tl 1800000
+mkdir -p $output_dir
+
+# Between 8pm (20) and 7am use night exposure mode.
+# However, the computer time is returning the hour 2 hours ahead,
+# so just adjust by adding 2.
+
+while true; do
+  current_hour=$(date +"%H")
+  if [[ "${current_hour#0}" -gt 20 || "${current_hour#0}" -lt 7 ]]; then
+    mode=night
+  fi
+  current_datetime=$(date +"%FT%H-%M-%S%Z")
+  raspistill -e png  -ex "$mode" --nopreview -o "$output_dir/${current_datetime}.png"
+  sleep $sleep_for
+done
